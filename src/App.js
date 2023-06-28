73ad753dc8de4dd43067f721ac2429f28478978b
ranamagdi <ranagrida@gmail.com>
2023-06-28 14:09:53 +0300

    deploy final

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Home from './Components/Home';
 
 function App() {
   return (
-    <div>
-    <Home/>
-    </div>
+    <HashRouter>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      </Routes>
+    </HashRouter>
   );
 }
 
